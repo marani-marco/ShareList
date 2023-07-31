9ef3121bd28a8dc0f68f833e71772ca7f14b4c1b
Marco Marani <marco.marani@aesys.tech>
2023-07-31 11:30:41 +0200

    Initial commit

---
+++ src/main/java/com/marco/sharelist/ShareListApplication.java
@@ -0,0 +1,13 @@
+package com.marco.sharelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marco/sharelist/ShareListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marco.sharelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
